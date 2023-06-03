5c17c4cc6bf36c60075c01cb1404fa7e52e1a6f0
Rafael Quevedo <31359531+rafaelrpq@users.noreply.github.com>
2023-06-02 23:22:00 -0300

    Update index.js

--- index.js
+++ index.js
@@ -119,7 +119,7 @@ setTimeout (()=> {
         let atualizado = dados.properties.meta.updated_at;
         footer.innerHTML = `Atualizado ${new Date(atualizado).toLocaleString ()}`
     })
-}, 200)
+}, 500)
 
 if ('serviceWorker' in navigator) {
     window.addEventListener ('load', () => {
