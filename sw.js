688645bb9db821c3c370738d5a463ed577acc3f4
Rafael Quevedo <rafaelrpq@gmail.com>
2023-06-04 10:30:09 -0300

    ..

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = 'clima-cache';
+var DYNAMIC_CACHE = 'clima-cache';
 var urlsToCache = [
   './index.html',
   './index.css',
@@ -9,7 +9,7 @@ var urlsToCache = [
 
 self.addEventListener('install', function(event) {
     event.waitUntil(
-        caches.open (CACHE_NAME)
+        caches.open (DYNAMIC_CACHE)
         .then(function(cache) {
             console.log('Opened cache');
             return cache.addAll(urlsToCache);
@@ -18,8 +18,24 @@ self.addEventListener('install', function(event) {
 });
 
 self.addEventListener ('fetch', (event) => {
-    event.respondWith (
-        caches.match (event.request)
-        .then (cacheResponse => (cacheResponse || fetch (event).request))
-    )
+    event.respondWith((async () => {
+      const cachedResponse = await caches.match(event.request);
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+
+      const response = await fetch(event.request);
+
+      if (!response || response.status !== 200 || response.type !== 'basic') {
+        return response;
+      }
+
+      if (urlsToCache) {
+        const responseToCache = response.clone();
+        const cache = await caches.open(DYNAMIC_CACHE)
+        await cache.put(event.request, response.clone());
+      }
+
+      return response;
+    })());
 })
